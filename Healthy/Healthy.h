955767096b1bf1a85d675fce6d62c4e72f20b96b
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-28 17:09:31 +0530

    Initial Commit

---
+++ Healthy/Healthy.h
@@ -0,0 +1,18 @@
+//
+//  Healthy.h
+//  Healthy
+//
+//  Created by Krisna Pranav on 28/04/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Healthy.
+FOUNDATION_EXPORT double HealthyVersionNumber;
+
+//! Project version string for Healthy.
+FOUNDATION_EXPORT const unsigned char HealthyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Healthy/PublicHeader.h>
+
+
