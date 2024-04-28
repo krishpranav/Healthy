7d41de6bf44a2e6855804de096bf022adc1a0525
Krisna Pranav <68631244+krishpranav@users.noreply.github.com>
2024-04-28 17:16:22 +0530

    Healthy: Protocols

---
+++ Healthy/Protocols.swift
@@ -0,0 +1,51 @@
+//
+//  Protocols.swift
+//  Healthy
+//
+//  Created by Krisna Pranav on 28/04/24.
+//
+
+import Foundation
+import LoggerAPI
+
+public enum InvalidDataError: Error {
+    case deserialization(String)
+    case serialization(String)
+}
+
+public enum State: String {
+    case UP
+    case DOWN
+}
+
+public class StatusDateFormatter {
+    private let dateFormatter: DateFormatter
+    
+    init() {
+        self.dateFormatter = DateFormatter()
+        self.dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        if let timeZone = TimeZone(identifier: "UTC") {
+            self.dateFormatter.timeZone = timeZone
+        } else {
+            Log.warning("UTC time zone not found.")
+        }
+    }
+    
+    public func string(from date: Date) -> String {
+        return dateFormatter.string(from: date)
+    }
+    
+    public func date(from string: String) -> Date? {
+        return dateFormatter.date(from: string)
+    }
+    
+    var dateFormat: String {
+        get {
+            return self.dateFormatter.dateFormat
+        }
+    }
+}
+
+public struct Status: Equatable {
+    
+}
